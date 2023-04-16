cabde2318439f7bc08c7f2ba074fa710bd79db1c
Zach Mutch <zkmutch@gmail.com>
2023-04-15 20:18:16 -0400

    added schema.sql and seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+-- Deletes then recreates the employee_tracker_db if one already exists
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department (id)
+);
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    -- manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (title)
+VALUES ('Engineering')
+       ('Finance')
+       ('Legal')
+       ('Sales');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Sales Lead, 100000, 4')
+       ('Salesperson, 80000, 4')
+       ('Lead Engineer, 150000, 1')
+       ('Software Engineer, 120000, 1')
+       ('Account Manager, 160000, 2')
+       ('Accountant, 125000, 2')
+       ('Legal Team Lead, 250000, 3')
+       ('Lawyer, 190000, 3');
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ('John, Doe, 1')
+       ('Mike, Chan, 2')
+       ('Ashley, Rodriguez, 3')
+       ('Kevin, Tupic, 4')
+       ('Kunal, Singh, 5')
+       ('Malia, Brown, 6')
+       ('Sarah, Lourd, 7')
+       ('Tom, Allen, 8');
+ 
